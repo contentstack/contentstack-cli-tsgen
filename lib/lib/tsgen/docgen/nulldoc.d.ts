ddb2fd5bfe61cb949020e09ef16f5a164b7ab095
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-11-12 14:25:11 +0530

    refactor: removed lib folder and updated .gitignore

--- lib/lib/tsgen/docgen/nulldoc.d.ts
+++
@@ -1,5 +0,0 @@
-import { DocumentationGenerator } from './doc';
-export default class NullDocumentationGenerator implements DocumentationGenerator {
-    interface(__description: string): null;
-    field(__description: string): null;
-}
--- lib/lib/tsgen/runner.d.ts
+++
@@ -1,4 +0,0 @@
-export default function tsgenRunner(outputFile: string, contentTypes: any[], prefix?: string, includeDocumentation?: boolean): Promise<{
-    definitions: number;
-    outputPath: string;
-}>;
