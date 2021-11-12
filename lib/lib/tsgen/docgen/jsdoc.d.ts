53210c6b59112565fe43797b1736a5e7c1e28ab8
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-11-12 14:25:11 +0530

    refactor: removed lib folder and updated .gitignore

--- lib/lib/tsgen/docgen/jsdoc.d.ts
+++
@@ -1,6 +0,0 @@
-import { DocumentationGenerator } from './doc';
-export default class JSDocumentationGenerator implements DocumentationGenerator {
-    interface(description: string): string | null;
-    field(description: string): string | null;
-    private block;
-}
