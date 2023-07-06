06f514762a19f980ba5be3bb251268cb3e421e37
Miles Bardon <milesbardon@gmail.com>
2023-07-06 08:59:20 +0100

    fix: reverting client changes

--- src/lib/stack/client.ts
+++ src/lib/stack/client.ts
@@ -1,95 +1,95 @@
-import * as http from "https";
-import * as async from "async";
-import { ContentTypeCollection } from "contentstack";
+import * as http from 'https'
+import * as async from 'async'
+import {ContentTypeCollection} from 'contentstack'
 
 type RegionUrlMap = {
   [prop: string]: string;
 };
 
 const REGION_URL_MAPPING: RegionUrlMap = {
-  na: "cdn.contentstack.io",
-  us: "cdn.contentstack.io",
-  eu: "eu-cdn.contentstack.com",
-  "azure-na": "azure-na-cdn.contentstack.com",
-};
+  na: 'cdn.contentstack.io',
+  us: 'cdn.contentstack.io',
+  eu: 'eu-cdn.contentstack.com',
+  'azure-na': 'azure-na-cdn.contentstack.com',
+}
 
 export type StackConnectionConfig = {
   apiKey: string;
   token: string;
   region: any;
   environment: string;
-  branch?: string | null;
-};
+  branch?: string|null;
+}
 
-const limit = 100;
+const limit = 100
 
 const queryParams = {
   limit,
   include_global_field_schema: true,
-};
+}
 
 export async function stackConnect(client: any, config: StackConnectionConfig) {
   try {
-    const clientParams: {
-      api_key: string;
-      delivery_token: string;
-      environment: string;
-      region: string;
-      branch?: string;
+    const clientParams: { 
+      api_key: string,
+      delivery_token: string,
+      environment: string,
+      region: string,
+      branch?: string
     } = {
       api_key: config.apiKey,
       delivery_token: config.token,
       environment: config.environment,
       region: config.region,
-    };
+    }
     if (config.branch) {
-      clientParams.branch = config.branch;
+      clientParams.branch = config.branch
     }
     // eslint-disable-next-line new-cap
-    const stack = client(clientParams);
+    const stack = client(clientParams)
 
     const results = (await stack.getContentTypes({
       ...queryParams,
       include_count: true,
-    })) as Omit<ContentTypeCollection, "count"> & { count: number };
+    })) as Omit<ContentTypeCollection, 'count'> & { count: number }
 
     if (results.count > limit) {
       const additionalQueries = Array.from(
-        { length: Math.ceil(results.count / limit) - 1 },
+        {length: Math.ceil(results.count / limit) - 1},
         (_, i) => {
           return async.reflect(async () => {
             return stack.getContentTypes({
               ...queryParams,
               skip: (i + 1) * limit,
-            });
-          });
+            })
+          })
         }
-      );
+      )
       const additionalResults = (await async.parallel(additionalQueries)) as {
         value: ContentTypeCollection;
-      }[];
+      }[]
 
       for (const r of additionalResults) {
         results.content_types = [
           ...results.content_types,
           ...r.value.content_types,
-        ];
+        ]
       }
     }
 
-    const types = results.content_types;
+    const types = results.content_types
 
     if (stack) {
       return {
         stack,
         types,
-      };
+      }
     }
-    throw new Error("Could not connect to the stack.");
+    throw new Error('Could not connect to the stack.')
   } catch (error) {
     throw new Error(
-      "Could not connect to the stack. Please check your credentials."
-    );
+      'Could not connect to the stack. Please check your credentials.'
+    )
   }
 }
 
@@ -99,44 +99,38 @@ export async function getGlobalFields(config: StackConnectionConfig) {
     return new Promise((resolve, reject) => {
       const options: any = {
         host: (REGION_URL_MAPPING as any)[config.region],
-        path: "v3/global_fields?include_branch=false",
+        path: 'v3/global_fields?include_branch=false',
         port: 443,
-        method: "GET",
+        method: 'GET',
         headers: {
           api_key: config.apiKey,
           access_token: config.token,
           environment: config.environment,
         },
-      };
-      if (config.branch) {
-        options.headers.branch = config.branch;
       }
-      const req = http.request(options, (res) => {
-        res.setEncoding("utf8");
-        let body = "";
-        res.on("data", (chunk) => {
-          body += chunk;
-        });
-        res.on("end", () => {
+      if (config.branch) {
+        options.headers.branch = config.branch
+      }
+      const req = http.request(options, res => {
+        res.setEncoding('utf8')
+        let body = ''
+        res.on('data', chunk => {
+          body += chunk
+        })
+        res.on('end', () => {
           if (res.statusCode === 200) {
-            resolve(JSON.parse(body));
+            resolve(JSON.parse(body))
           } else {
-            reject(
-              new Error(
-                "Could not connect to the stack. Please check your credentials."
-              )
-            );
+            reject(new Error('Could not connect to the stack. Please check your credentials.'))
           }
-        });
-      });
-      req.on("error", (error) => {
-        reject(error);
-      });
-      req.end();
-    });
+        })
+      })
+      req.on('error', error => {
+        reject(error)
+      })
+      req.end()
+    })
   } catch (error) {
-    throw new Error(
-      "Could not connect to the stack. Please check your credentials."
-    );
+    throw new Error('Could not connect to the stack. Please check your credentials.')
   }
 }
