f8e413bf81e636793b45a626f1a8fb8e9493cde5
raj pandey <raj.pandey@contentstack.com>
2024-09-25 15:06:57 +0530

    fixed current test cases

--- test/unit/commands/tsgen.test.ts
+++ test/unit/commands/tsgen.test.ts
@@ -1,8 +1,7 @@
-import {test} from '@oclif/test'
-import {configHandler} from '@contentstack/cli-utilities'
-
+import { test } from '@oclif/test'
+import { configHandler } from '@contentstack/cli-utilities'
 import * as mock from '../mock/common.mock.json'
-
+import { mock_data } from '../mock/mock2';
 const GRAPHQL_REGION_URL_MAPPING = {
   na: 'https://graphql.contentstack.com',
   us: 'https://graphql.contentstack.com',
@@ -12,37 +11,38 @@ const GRAPHQL_REGION_URL_MAPPING = {
 }
 
 const region: { cma: string; name: string; cda: string } = configHandler.get('region')
-const graphqlUrl: string = region?.name && GRAPHQL_REGION_URL_MAPPING[region.name.toLowerCase() as keyof  typeof GRAPHQL_REGION_URL_MAPPING]
+const graphqlUrl: string = region?.name && GRAPHQL_REGION_URL_MAPPING[region.name.toLowerCase() as keyof typeof GRAPHQL_REGION_URL_MAPPING]
 const alias = 'token1'
-configHandler.set(`tokens.${alias}`,  mock.tokens[alias])
+configHandler.set(`tokens.${alias}`, mock.tokens[alias])
 const filePath = './generate1.d.ts'
+
 
 describe('Tsgen command', () => {
   describe('Graphql type generator', () => {
     test
-    .stdout({print: process.env.PRINT === 'true' || false})
-    .nock(graphqlUrl, api =>
-      api
-      .post(`/stacks/${mock.tokens[alias].apiKey}`)
-      .reply(200, {
-        data: mock.output,
-      })
-    )
-    .command(['tsgen', '--token-alias', alias, '--output', filePath, '--api-type', 'graphql'])
-    .it('should generate a type def for graphql query')
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .nock(graphqlUrl, api =>
+        api
+          .post(`/stacks/${mock.tokens[alias].apiKey}?environment=development`)
+          .reply(200, {
+            data: mock_data,
+          })
+      )
+      .command(['tsgen', '--token-alias', alias, '--output', filePath, '--api-type', 'graphql'])
+      .it('should generate a type def for graphql query')
   })
 
   describe('Graphql type generator with namespace', () => {
     test
-    .stdout({print: process.env.PRINT === 'true' || false})
-    .nock(graphqlUrl, api =>
-      api
-      .post(`/stacks/${mock.tokens[alias].apiKey}`)
-      .reply(200, {
-        data: mock.output,
-      })
-    )
-    .command(['tsgen', '--api-type', 'graphql', '-a', alias, '--output', filePath, '--namespace', 'GraphQL'])
-    .it('should generate a type def for graphql query with namespace')
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .nock(graphqlUrl, api =>
+        api
+          .post(`/stacks/${mock.tokens[alias].apiKey}?environment=development`)
+          .reply(200, {
+            data: mock_data,
+          })
+      )
+      .command(['tsgen', '--api-type', 'graphql', '-a', alias, '--output', filePath, '--namespace', 'GraphQL'])
+      .it('should generate a type def for graphql query with namespace')
   })
 })
