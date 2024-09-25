7948043ce580c1ffd98d7a2a32d8d2b617a4a8a5
raj pandey <raj.pandey@contentstack.com>
2024-09-25 17:32:12 +0530

    fixed the test cases for oclif test v4

--- test/unit/commands/tsgen.test.ts
+++ test/unit/commands/tsgen.test.ts
@@ -1,5 +1,7 @@
-import {test} from '@oclif/test'
+import {expect} from 'chai'
 import {configHandler} from '@contentstack/cli-utilities'
+import fancy from 'fancy-test'
+import {runCommand} from '@oclif/test'
 
 import * as mock from '../mock/common.mock.json'
 
@@ -19,8 +21,8 @@ const filePath = './generate1.d.ts'
 
 describe('Tsgen command', () => {
   describe('Graphql type generator', () => {
-    test
-    .stdout({print: process.env.PRINT === 'true' || false})
+    fancy
+    .stdout({print: process.env.PRINT === 'true' || true})
     .nock(graphqlUrl, api =>
       api
       .post(`/stacks/${mock.tokens[alias].apiKey}?environment=development`)
@@ -28,12 +30,13 @@ describe('Tsgen command', () => {
         data: mock.mock_data,
       })
     )
-    .command(['tsgen', '--token-alias', alias, '--output', filePath, '--api-type', 'graphql'])
-    .it('should generate a type def for graphql query')
+    .it('should generate a type def for graphql query', async () => {
+      await runCommand(['tsgen', '--token-alias', alias, '--output', filePath, '--api-type', 'graphql'], { root: process.cwd() });
+    })
   })
 
   describe('Graphql type generator with namespace', () => {
-    test
+    fancy
     .stdout({print: process.env.PRINT === 'true' || false})
     .nock(graphqlUrl, api =>
       api
@@ -42,7 +45,8 @@ describe('Tsgen command', () => {
         data: mock.mock_data,
       })
     )
-    .command(['tsgen', '--api-type', 'graphql', '-a', alias, '--output', filePath, '--namespace', 'GraphQL'])
-    .it('should generate a type def for graphql query with namespace')
+    .it('should generate a type def for graphql query with namespace', async () => {
+      await runCommand(['tsgen', '--api-type', 'graphql', '-a', alias, '--output', filePath, '--namespace', 'GraphQL'], { root: process.cwd() });
+    })
   })
 })
