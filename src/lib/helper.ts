82290ec1e83e08c7919900b7b523f2e9f06aa7eb
raj pandey <raj.pandey@contentstack.com>
2025-01-17 10:26:34 +0530

    fixed path traversal issue

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,1 +1,5 @@
-export const sanitizePath = (str: string) => str?.replace(/^(\.\.(\/|\\|$))+/, '');
+export const sanitizePath = (str: string) =>
+  str
+    ?.replace(/^([\/\\])+/, "./") // Normalize leading slashes/backslashes to './'
+    .replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
+    .replace(/(\.\.(\/|\\|$))+/g, ""); // Remove directory traversal (../ or ..\)
