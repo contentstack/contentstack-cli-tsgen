e25ee2e1a5599b9afc9075932cbc084758455d5d
raj pandey <raj.pandey@contentstack.com>
2025-02-05 15:15:07 +0530

    file update

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,6 +1,5 @@
 export const sanitizePath = (str: string) => {
     return str
-    ?.replace(/^([\/\\])+/, "./") // Normalize leading slashes/backslashes to ''
-    .replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
+    ?.replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
     .replace(/(\.\.(\/|\\|$))+/g, ""); // Remove directory traversal (../ or ..\)
 }
