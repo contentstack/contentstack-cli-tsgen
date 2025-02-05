e28211ef62cbbf6185cf82508624386d835dfae7
raj pandey <raj.pandey@contentstack.com>
2025-02-05 13:10:07 +0530

    revert fix

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,9 +1,7 @@
 export const sanitizePath = (str: string) => {
-  return str;
-  // const decodedString = decodeURI(str)
-  // return decodedString
-  //   ?.replace(/^([\/\\])+/, "") // Normalize leading slashes/backslashes to ''
-  //   .replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
-  //   .replace(/(\.\.(\/|\\|$))+/g, ""); // Remove directory traversal (../ or ..\)
+  return str
+    ?.replace(/^([\/\\])+/, "") // Normalize leading slashes/backslashes to ''
+    .replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
+    .replace(/(\.\.(\/|\\|$))+/g, ""); // Remove directory traversal (../ or ..\)
 }
   
